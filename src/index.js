1f6a1b7ad1c91909d13c1feff01ab33df70033ec
LucasMorais1998 <lucas14.morais@gmail.com>
2023-09-11 13:46:54 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -75,18 +75,14 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  const { username } = request.headers;
-
-  const { todos } = users.find((user) => user.username === username);
+  const { todos } = request.user;
 
   return response.json(todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
   const { title, deadline } = request.body;
-  const { username } = request.headers;
-
-  const user = users.find((user) => user.username === username);
 
   const newTodo = {
     id: uuidv4(),
@@ -102,11 +98,9 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  const { username } = request.headers;
+  const { user } = request;
   const { title, deadline } = request.body;
   const { id } = request.params;
-
-  const user = users.find((user) => user.username === username);
 
   const todo = user.todos.find((todo) => todo.id === id);
 
@@ -115,16 +109,14 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   }
 
   todo.title = title;
-  todo.deadline = deadline;
+  todo.deadline = new Date(deadline);
 
   return response.json(todo);
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  const { username } = request.headers;
+  const { user } = request;
   const { id } = request.params;
-
-  const user = users.find((user) => user.username === username);
 
   const todo = user.todos.find((todo) => todo.id === id);
 
@@ -132,16 +124,14 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
     return response.status(404).json({ error: "todo not found!" });
   }
 
-  todo.done = true;
+  todo.done = !todo.done;
 
   return response.json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  const { username } = request.headers;
+  const { user } = request;
   const { id } = request.params;
-
-  const user = users.find((user) => user.username === username);
 
   const todoIndex = user.todos.findIndex((todo) => todo.id === id);
 
